ef3fb151206c58ddde810771f7b7d71f48d8bcf9
KrishnasaiM-Hexaware <KrishnasaiM@hexaware.com>
2018-06-04 14:07:47 +0530

    xx

--- src/main/java/com/billPayment/MainClass.java
+++ src/main/java/com/billPayment/MainClass.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MainClass {
-//created to start application1
+//created to start application1sdghadfhjasdfhjauas
 	@Bean 
 	public SessionFactory setSessionFactory(HibernateEntityManagerFactory hemf) {
 		return hemf.getSessionFactory();
